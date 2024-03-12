b79fee2f737643c2c02c73c7688a90fdbb7f14c0
AlexxSome <alex.soto.29.9.88@gmail.com>
2024-03-12 20:36:29 -0300

    get response api

--- js/index.js
+++ js/index.js
@@ -1,18 +1,38 @@
 const INIT_URL ="https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150";
+const getList = getData(INIT_URL);
 
 async function init() {
-    let pokemonContainer = document.getElementById("pokemonContainer");
-    let img = document.createElement("img");
-    const getList = getData(INIT_URL);
-    pokemonContainer.appendChild(img);
-
     getList.then((result) => {
-        console.log('El resultado es:', result.results); // Imprimirá: El resultado es: 42
+        localStorage.setItem('pokeList', JSON.stringify(result.results));
+        firstPage();
     }).catch((error) => {
         console.error('Ocurrió un error:', error);
     });
     //img.setAttribute("src",)
+}
 
+function firstPage(){
+    setTimeout(()=>{
+        let pokemonContainer = document.getElementById("pokemonContainer");
+
+        let list = JSON.parse(localStorage.getItem("pokeList"));
+
+        list.forEach(pokemon => {
+            let img = document.createElement("img");
+            console.log("poke: ", pokemon)
+            let getPokemon = getData(pokemon.url)
+
+            getPokemon.then((poke) => {
+                img.setAttribute("src", poke.sprites.front_default);
+            }).catch((error) => {
+                console.error('Ocurrió un error:', error);
+            });
+
+
+            pokemonContainer.appendChild(img);
+        });
+
+    }, 500)
 }
 
 async function getData(url = '') {
